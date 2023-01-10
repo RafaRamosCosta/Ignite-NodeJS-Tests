1905dbdbfcb2f36bc8bfa54c369347197064e220
RafaRamosCosta <rm94281@fiap.com.br>
2023-01-10 11:00:41 -0300

    refactor: adds transfer type validation and assigns recipient & sender id.

--- src/modules/statements/useCases/createStatement/CreateStatementController.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.ts
@@ -1,29 +1,35 @@
-import { Request, Response } from 'express';
-import { container } from 'tsyringe';
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { OperationType } from "../../entities/Statement";
 
-import { CreateStatementUseCase } from './CreateStatementUseCase';
-
-enum OperationType {
-  DEPOSIT = 'deposit',
-  WITHDRAW = 'withdraw',
-}
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
 
 export class CreateStatementController {
   async execute(request: Request, response: Response) {
     const { id: user_id } = request.user;
     const { amount, description } = request.body;
 
-    const splittedPath = request.originalUrl.split('/')
+    const splittedPath = request.originalUrl.split("/");
     const type = splittedPath[splittedPath.length - 1] as OperationType;
 
     const createStatement = container.resolve(CreateStatementUseCase);
 
-    const statement = await createStatement.execute({
+    const statementInfo = {
       user_id,
       type,
       amount,
-      description
-    });
+      description,
+    };
+
+    if (type === OperationType.TRANSFER) {
+      const { user_id: recipient_id } = request.params;
+      Object.assign(statementInfo, {
+        recipient_id,
+        sender_id: user_id,
+      });
+    }
+
+    const statement = await createStatement.execute(statementInfo);
 
     return response.status(201).json(statement);
   }
